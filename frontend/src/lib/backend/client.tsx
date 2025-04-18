d227dd1136b7772a163496fb3951788aef74a818
socra167 <socra167@gmail.com>
2025-04-18 15:41:06 +0900

    localhost 하드코딩 URL 변경

--- frontend/src/lib/backend/client.tsx
+++ frontend/src/lib/backend/client.tsx
@@ -2,11 +2,11 @@ import { paths } from "@/lib/backend/apiV1/schema";
 import createClient from "openapi-fetch";
 
 const clientWithNoHeaders = createClient<paths>({
-  baseUrl: "http://localhost:8080",
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 const client = createClient<paths>({
-  baseUrl: "http://localhost:8080",
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
